4152f62ddc893cf0e17fae8c2b9317f3029de161
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-27 11:55:41 +0700

    Create index.js

---
+++ core/index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDBJgF1WwtGveoiCbEpdrJ8ZEsvNurlhcI",
+  authDomain: "me-voting.firebaseapp.com",
+  databaseURL: "https://me-voting-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "me-voting",
+  storageBucket: "me-voting.appspot.com",
+  messagingSenderId: "95946629295",
+  appId: "1:95946629295:web:960b0fe2871a9acc72b45a",
+  measurementId: "G-FRV5YKE1ZY"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
