6d7e9aaa75ac82bf81fd93e1c7ea8cebe6f8131d
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-29 05:38:02 +0700

    Add files via upload

---
+++ apikey/js/index.js
@@ -0,0 +1,12 @@
+var uri_wapi = "http://mevoting.is-best.net/live/?api_key=3dbd017cb1eb3021046943bad6cf31db";
+fetch(uri_wapi, {
+    method: 'post',
+    mode: 'same-origin',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(response => console.log(JSON.stringify(response)))
+console.log(uri_wapi);
